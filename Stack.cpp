74ae192265108682703b83f22515420391771f91
IzKirill <izmailov.ka@phystech.edu>
2023-09-19 22:38:00 +0300

    add Stack push and pop

--- Stack.cpp
+++ Stack.cpp
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
+#include <malloc.h>
 #include "Stack.h"
 
 int StackCtor(Steck* stk, const size_t Capacity)
@@ -16,6 +17,45 @@ int StackCtor(Steck* stk, const size_t Capacity)
         STACK_DUMP(stk);
 
     return 0;
+}
+
+int StackPush(Steck* stk, Elemt value)
+{
+    if(StackOK(stk))
+        STACK_DUMP(stk);
+
+    if (stk->Size == stk->Capacity)
+    {
+        stk->Capacity *= 2;
+        stk->data = (Elemt*) realloc(stk->data, stk->Capacity*sizeof(Elemt));
+        for (int i = stk->Size; i < stk->Capacity; i++)
+            stk->data[i] = 0;
+    }
+
+    stk->data[stk->Size++] = value;
+
+    if(StackOK(stk))
+        STACK_DUMP(stk);
+
+    return 0;
+}
+
+int StackPop(Steck* stk, Elemt* refValue)
+{
+    if(StackOK(stk))
+        STACK_DUMP(stk);
+
+    if (2*stk->Size <= stk->Capacity)
+    {
+        stk->Capacity /= 2;
+        free(stk->data+stk->Capacity);
+    }
+
+    *refValue = stk->data[--stk->Size];
+    stk->data[stk->Size] = 0;
+
+    if(StackOK(stk))
+        STACK_DUMP(stk);
 }
 
 int StackDtor(Steck* stk)
@@ -42,12 +82,12 @@ int StackDtor(Steck* stk)
 
 int StackOK(Steck* stk)
 {
-    if (stk == NULL) return 1;
+    if (stk == nullptr) return 1;
     if (stk->Size < 0) return 2;
     if (stk->Capacity < 0) return 3;
     if (stk->Capacity < stk->Size) return 4;
     for (int i = 0; i < stk->Size; i++)
-        if (stk->data[i] == NAN || stk->data[i] == NULL) return 5;
+        if (stk->data[i] == NAN || stk->data[i] == 0) return 5;
     return 0;
 }
 
@@ -60,6 +100,9 @@ int StackDump(Steck* stk, const size_t nline, const char* namefile, const char* 
     for (int i = 0; i < stk->Size; i++)
         printf("*[%d]=%d\n\t", i, stk->data[i]);
 
-    printf("}\n  }");
+    for (int j = stk->Size; j < stk->Capacity; j++)
+        printf(" [%d]=%d\n\t", j, stk->data[j]);
+
+    printf("}\n  }\n");
     return 0;
 }
